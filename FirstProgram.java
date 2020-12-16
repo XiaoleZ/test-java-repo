fc4ececce5b8b407d8501e45a6513de15ff10048
XiaoleZ <xiaole2@ualberta.ca>
2020-12-15 20:00:22 -0800

    add a getMaxValue method to return the maxValue in a given number array

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,18 @@
 class Sample{
     public static void main(String args[]){
         System.out.println("Hello World!! This is Java code.");
+
+        int numbers[] = {0,1,-10,32,44,11,55,-7,99};
+        System.out.println("Maximum Value = " + getMaxValue(numbers));
     }
+
+    public static int getMaxValue(int[] numbers){
+        int maxValue = numbers[0];
+        for(int i=1; i<numbers.length;i++){
+            if(numbers[i] > maxValue){
+                maxValue = numbers[i];
+            }
+        }
+        return maxValue;
+    } 
 }
