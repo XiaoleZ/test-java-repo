b0319e459ac61a6f4ed98b1d44a9d9098cf87733
XiaoleZ <xiaole2@ualberta.ca>
2020-12-15 20:04:24 -0800

    commit that adds another parameter to getMaxValue method

--- FirstProgram.java
+++ FirstProgram.java
@@ -3,13 +3,14 @@ class Sample{
         System.out.println("Hello World!! This is Java code.");
 
         int numbers[] = {0,1,-10,32,44,11,55,-7,99};
-        System.out.println("Maximum Value = " + getMaxValue(numbers));
+        int maxRange = 50;
+        System.out.println("Maximum Value under " + maxRange + " = " + getMaxValue(numbers,maxRange));
     }
 
-    public static int getMaxValue(int[] numbers){
+    public static int getMaxValue(int[] numbers, int maxRange){
         int maxValue = numbers[0];
         for(int i=1; i<numbers.length;i++){
-            if(numbers[i] > maxValue){
+            if(numbers[i] > maxValue && numbers[i] <= maxRange){
                 maxValue = numbers[i];
             }
         }
